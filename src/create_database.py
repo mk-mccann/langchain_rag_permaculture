15a3dd52a5a028af444085eb220ffae5a9ea300c
Matthew McCann <9754965+mk-mccann@users.noreply.github.com>
2025-09-10 08:01:25 +0200

    Lazy loading for mixed doc formats

--- src/create_database.py
+++ src/create_database.py
@@ -1,64 +1,48 @@
-from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
-from langchain_text_splitters import MarkdownHeaderTextSplitter
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_text_splitters import MarkdownHeaderTextSplitter, RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_mistralai.embeddings import MistralAIEmbeddings
 from langchain.schema import Document
-import mistralai
+from mistralai import Mistral
 import dotenv
 import uuid
 import os
+import asyncio
 
 
-# Set the system MistralAI API key
-mistralai.api_key = os.environ["MISTRAL_API_KEY"]
+# Load the environment variables and set the system MistralAI API key
+dotenv.load_dotenv()
+mistral_api_key = os.environ["MISTRAL_API_KEY"]
+client = Mistral(api_key=mistral_api_key)
 
-# Set paths to data source and local chroma database
+# Set paths to data source(s) and local chroma database
 DATA_PATH = "./data/books"
 CHROMA_PATH = "./chroma"
 
 
 def load_documents(path: str) -> list[Document]:
     '''
-    This function loads all markdown files from the specified directory and returns them as a list of Document objects.
+    This function loads all files from the specified directory and returns them as a list of Document objects.
+    Here I'm using the DirectoryLoader to load all files from the specified directory without needing to 
+    specify the filetype. By default I show a progress bar and use multithreading to speed up the loading process.
     '''
-
-    loader = DirectoryLoader(path, glob="*.md")
+    loader = DirectoryLoader(path, show_progress=True, use_multithreading=True)
     documents = loader.load()
     return documents
 
 
-def load_pdf_documents(path: str) -> list[Document]:
+def split_documents(documents:list[Document]) -> list[Document]:
     '''
-    This function loads all PDF files from the specified directory and returns them as a list of Document objects.
-    '''
-
-    loader = DirectoryLoader(path, glob="*.pdf", loader_cls=PyPDFLoader)
-    documents = loader.load()
-    return documents
-
-
-def split_documents(documents:list[Document], splitter='md') -> list[Document]:
-    '''
-    Here I want to split the document(s) into smaller chunks of semanitically related text.
-    I'm choosing to use the MarkdownHeaderTextSplitter by default to split the documents by headers, since I'm using a 
-    .md format for my books. However, if the documents were in a different format, I could use 
-    the RecursiveCharacterTextSplitter to split based on characters, sentences, or paragraphs.  
+    Here I want to split the document(s) into smaller chunks of semantically related text.
+    I'm choosing to use the MarkdownHeaderTextSplitter by default to split the documents by headers, 
+    since I'm using a .md format for my books. However, if the documents were in a different format, 
+    I could use the RecursiveCharacterTextSplitter to split based on characters, sentences, or paragraphs.
     '''
 
-    if splitter == 'md':
-        text_splitter = MarkdownHeaderTextSplitter(
-            headers_to_split_on=[
-                "#", "##", "###", "####", "#####", "######"
-            ],
-            chunk_size=500,
-            chunk_overlap=50
-        )
-    else:
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=500,
-            chunk_overlap=50,
-            separators=["\n\n", "\n", " ", ""]
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50,
+        separators=["\n\n", "\n", " ", ""]
         )
 
 
